5e8793738f7d78cbed29a84a8b9d773a9b73dcd2
Dante Novoa <dante.novoa@minted.com>
2024-12-07 14:07:16 -0600

    app working, missing button to download changes

--- app/crud.py
+++ app/crud.py
@@ -1,16 +1,27 @@
 from sqlalchemy.orm import Session
 from .models import Participant
 from .schemas import ParticipantCreate, ParticipantUpdate
+from app.logger.logger import logger
+from uuid import uuid4
 
 def create_participant(db: Session, participant: ParticipantCreate):
     db_participant = Participant(**participant.dict())
+    generated_id = uuid4()
+    generated_id_str = str(generated_id)
+    short_id = generated_id_str[:6]
+    aztlan_id = f"{short_id}"
+
+    db_participant.aztlan_id = aztlan_id
+    logger.info(f"Creando participante {db_participant.aztlan_id}")
+
     db.add(db_participant)
     db.commit()
     db.refresh(db_participant)
+
     return db_participant
 
-def get_participant(db: Session, participant_id: int):
-    return db.query(Participant).filter(Participant.id == participant_id).first()
+def get_participant_by_aztlan_id(db: Session, aztlan_id: str):
+    return db.query(Participant).filter(Participant.aztlan_id == aztlan_id).first()
 
 def update_participant(db: Session, participant_id: int, updates: ParticipantUpdate):
     db_participant = get_participant(db, participant_id)
